107be445c4a49602e16360f2e3695d9d6b44f33d
gokulnfonics <gokul@nfonics.com>
2024-08-22 11:15:24 +0530

    added changes in model campus and department

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'department';
+    protected $fillable = ['department_name', 'department_code', 'address', 'campus_id'];
+
+    public function campus(){
+        return $this->belongsTo(Campus::class,'campus_id');
+    }
 }

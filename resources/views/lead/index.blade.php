7f7937d08c723f874e55054a7b57b3443cb6a3fd
gopakumar-nfonics <gopakumar@nfonics.com>
2024-09-13 10:12:57 +0530

    Approvel message

--- resources/views/lead/index.blade.php
+++ resources/views/lead/index.blade.php
@@ -235,7 +235,7 @@ $(document).ready(function() {
 function approve(proid) {
     swal({
             title: "Are you sure?",
-            text: "You want to approve this proposal",
+            text: "Once the approval process is completed, the RO will be generated and sent to the vendor's registered email address.",
             icon: "warning",
             buttons: true,
             dangerMode: true,

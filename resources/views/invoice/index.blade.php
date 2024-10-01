b6fad20b85a590a6fd28f0a9cf40497a002c4989
arun0580 <arun.raj@nfonics.com>
2024-10-01 19:15:35 +0530

    Menu change

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -89,7 +89,7 @@
                                                         </span>
                                                         <!--end::Svg Icon-->Payment initiated
                                                     </span>
-                                                    
+
                                                     @else
                                                     <span class="badge badge-light-success fs-8">
                                                         <!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
@@ -189,13 +189,6 @@
                                         <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-200px py-4"
                                             data-kt-menu="true">
 
-
-
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="{{ route('lead.show',1) }}" class="menu-link px-3">View </a>
-                                            </div>
-                                            <!--end::Menu item-->
                                             <!--begin::Menu item-->
                                             @if(!Auth::user()->isvendor() && $inv->invoice_status ==0)
                                             <div class="menu-item px-3">
@@ -204,6 +197,14 @@
                                             </div>
                                             @endif
                                             <!--end::Menu item-->
+
+                                            <!--begin::Menu item-->
+                                            <div class="menu-item px-3">
+                                                <a href="{{ route('invoice.show',$inv->id) }}"
+                                                    class="menu-link px-3">View </a>
+                                            </div>
+                                            <!--end::Menu item-->
+
                                             <!--begin::Menu item-->
                                             <div class="menu-item px-3">
                                                 <a href="" class="menu-link px-3">Edit</a>

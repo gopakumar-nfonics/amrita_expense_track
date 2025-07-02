7c01e03e58d15158214fccadd9033d9eb95f5581
arun0580 <arun.raj@nfonics.com>
2025-07-02 16:00:56 +0530

    text change

--- resources/views/travelexpense/index.blade.php
+++ resources/views/travelexpense/index.blade.php
@@ -10,7 +10,7 @@
                 <!--begin::Page title-->
                 <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
                     <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
-                        Expense Listing
+                        Travel Expense Listing
                     </h1>
                 </div>
             </div>

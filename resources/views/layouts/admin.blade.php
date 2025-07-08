53342df4983417a8430d48577a7a70d92819f237
arun0580 <arun.raj@nfonics.com>
2025-07-08 14:05:16 +0530

    menu added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -347,6 +347,18 @@
                                                 <!--end:Menu link-->
                                             </div>
                                             <!--end:Menu item-->
+                                               <!--begin:Menu item-->
+                                            <div class="menu-item">
+                                                <!--begin:Menu link-->
+                                                <a class="menu-link" href="{{ route('staffreport') }}">
+                                                    <span class="menu-bullet">
+                                                        <span class="bullet bullet-dot"></span>
+                                                    </span>
+                                                    <span class="menu-title">Staff Wise</span>
+                                                </a>
+                                                <!--end:Menu link-->
+                                            </div>
+                                            <!--end:Menu item-->
 
                                         </div>
                                         <!--end:Menu sub-->
